74d98327bb2417bca705142b0492850c3e0c90d1
huang <401756536@qq.com>
2023-11-05 19:19:27 +0800

    push commit

--- src/main/java/com/huang/git/gittest.java
+++ src/main/java/com/huang/git/gittest.java
@@ -2,11 +2,11 @@ package com.huang.git;
 
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
